41191aea9573650e817eda0d785d79cafc8e8f7d
Ostebn <austin.jian07@gmail.com>
2024-05-18 10:26:55 -0400

    Update game a little

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function play(){
             }else{
                 if(bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width && bird_props.left + bird_props.width > pipe_sprite_props.left && bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height && bird_props.top + bird_props.height > pipe_sprite_props.top){
                     game_state = 'End';
-                    message.innerHTML = 'Sad!!! Game is over'.fontcolor('red') + '<br>Press Enter To Restart';
+                    message.innerHTML = 'GAME OVER'.fontcolor('red') + '<br>Your score is: '+ score_val.innerHTML + '<br>Press Enter To Restart';
                     message.classList.add('messageStyle');
                     img.style.display = 'none';
                     sound_die.play();
