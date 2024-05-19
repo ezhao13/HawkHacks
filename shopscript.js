6a38e98330425cd1a05578138389dfdf53d91a3f
Ostebn <austin.jian07@gmail.com>
2024-05-18 21:25:52 -0400

    Push the debugged

--- shopscript.js
+++ shopscript.js
@@ -1,25 +1,46 @@
+let fatHawkOwned = false;
+let smallHawkOwned = false;
+let equippedHawk = 'default';
+
 function buyFatHawk() {
-    var button = document.getElementById('buyfathawk');
-    button.innerHTML="Equip";
-    button.setAttribute("onClick", "javascript: equipFatHawk();");
-    document.getElementById('pricefat').innerHTML = ("Owned");
+    if (!fatHawkOwned) {
+        fatHawkOwned = true;
+        document.getElementById('buyfathawk').innerHTML = "Equip";
+        document.getElementById('pricefat').innerHTML = "Owned";
+        document.getElementById('buyfathawk').setAttribute("onClick", "equipFatHawk()");
+    }
 }
 
 function buySmallHawk() {
-    var button = document.getElementById('buysmallhawk');
-    button.innerHTML="Equip";
-    button.setAttribute("onClick", "javascript: equipFatHawk();");
-    document.getElementById('pricesmall').innerHTML = ("Owned");
+    if (!smallHawkOwned) {
+        smallHawkOwned = true;
+        document.getElementById('buysmallhawk').innerHTML = "Equip";
+        document.getElementById('pricesmall').innerHTML = "Owned";
+        document.getElementById('buysmallhawk').setAttribute("onClick", "equipSmallHawk()");
+    }
 }
 
 function equipDefault() {
-    alert("work")
+    equippedHawk = 'default';
+    updateUI();
 }
 
 function equipFatHawk() {
-    alert("work")
+    if (fatHawkOwned) {
+        equippedHawk = 'fat';
+        updateUI();
+    }
 }
 
 function equipSmallHawk() {
-    alert("work")
+    if (smallHawkOwned) {
+        equippedHawk = 'small';
+        updateUI();
+    }
+}
+
+function updateUI() {
+    document.getElementById('pricedefault').innerHTML = equippedHawk === 'default' ? "Equipped" : "Owned";
+    document.getElementById('pricefat').innerHTML = equippedHawk === 'fat' ? "Equipped" : (fatHawkOwned ? "Owned" : "Price: 30 tokens");
+    document.getElementById('pricesmall').innerHTML = equippedHawk === 'small' ? "Equipped" : (smallHawkOwned ? "Owned" : "Price: 50 tokens");
 }
